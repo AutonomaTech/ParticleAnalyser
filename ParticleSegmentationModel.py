12e112518154ca4af691cc7c4416b5b859fbb009
andrea <marcosano.andrea@gmail.com>
2024-11-19 12:45:03 +0800

    added plotting feature

--- ParticleSegmentationModel.py
+++ ParticleSegmentationModel.py
@@ -60,7 +60,7 @@ class ParticleSegmentationModel:
         self.stability_score_offset = 0.8
         self.crop_n_layers = 2
         self.crop_n_points_downscale_factor = 2
-        self.min_mask_region_area = 0.5
+        self.min_mask_region_area = 0.0
         self.box_nms_tresh = 0.9
         self.use_m2m = True,
 
